85bfd63890002926e2b3b3379bdaeb7207b686cd
Pastew <kpastew@gmail.com>
2019-06-10 19:16:56 +0200

    Lighting up related fields works!

---
+++ Assets/BetField.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BetField : MonoBehaviour
+{
+    public List<BetField> relatedFields;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void TurnLights(bool turnedOn)
+    {
+        foreach(BetField field in relatedFields)
+        {
+            field.TurnLight(turnedOn);
+        }
+    }
+
+    private void TurnLight(bool turnedOn)
+    {
+        transform.GetChild(0).gameObject.SetActive(turnedOn);
+    }
+}
---
+++ Assets/ScriptsFrontend/Hand.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class Hand : MonoBehaviour
+{
+    private GameObject heldChip;
+    private BetField closestBetField;
+
+    private List<BetField> betFields;
+
+    private void Start()
+    {
+        betFields = FindObjectsOfType<BetField>().ToList();
+    }
+
+    void Update()
+    {
+        Vector3 newPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        newPos.z = 0;
+        transform.position = newPos;
+
+        if(heldChip != null)
+        {
+            BetField newClosestBetField = FindClosestBetField();
+            print(newClosestBetField.name);
+            if (newClosestBetField != closestBetField)
+            {
+                if(closestBetField != null)
+                    closestBetField.TurnLights(false);
+
+                closestBetField = newClosestBetField;
+                closestBetField.TurnLights(true);
+            }
+        }
+
+        if (Input.GetMouseButtonUp(0) && heldChip != null)
+        {
+            PlaceChip();
+        }
+    }
+
+    private BetField FindClosestBetField()
+    {
+        float minDistance = float.PositiveInfinity;
+        BetField currentClosestBetField = null;
+
+        foreach (BetField betField in betFields)
+        {
+            float distance = Vector2.Distance(betField.transform.position, transform.position);
+            if (distance < minDistance)
+            {
+                minDistance = distance;
+                currentClosestBetField = betField;
+            }
+        }
+        return currentClosestBetField;
+    }
+
+    private void PlaceChip()
+    {
+        heldChip.transform.parent = null;
+    }
+
+    public void SpawnChip(GameObject chipPreset)
+    {
+        heldChip = Instantiate(chipPreset, transform);
+    }
+}
