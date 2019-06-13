0b4515e339cdb0df350a788abb6e094ceb5708bf
Pastew <kpastew@gmail.com>
2019-06-13 08:09:31 +0200

    Refactor: Removed unused file

--- Assets/ScriptsRouletteBackend/Wheel.cs
+++ Assets/ScriptsRouletteBackend/Wheel.cs
@@ -1,1 +1,29 @@
-﻿
+﻿using System;
+
+public interface IWheel
+{
+    int Spin();
+}
+
+public class SimpleRandomWheel : IWheel
+{
+    public int Spin()
+    {
+        return new Random().Next(0, 36 + 1);
+    }
+}
+
+public class FakeWheelReturningGivenNumber : IWheel
+{
+    private int alywasReturnedValue;
+
+    public FakeWheelReturningGivenNumber(int alwaysReturnedValue)
+    {
+        this.alywasReturnedValue = alwaysReturnedValue;
+    }
+
+    public int Spin()
+    {
+        return alywasReturnedValue;
+    }
+}
